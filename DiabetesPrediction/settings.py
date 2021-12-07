03965d56b0a8a989aed4d7656510771255d8e3d8
Srishti Ahuja <57188326+Srishti-Ahuja@users.noreply.github.com>
2021-12-07 17:35:43 +0530

    Update settings.py

--- DiabetesPrediction/settings.py
+++ DiabetesPrediction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gkfcyng@vvdy6qu-*_-z6yw&%x91jk%-s3!wk)*i&a5faum&@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['diabetespredictionapp.azurewebsites.net','*']
 
