e3bd0945bd1b8ed119b858ca5f931399edceb367
Srishti Ahuja <57188326+Srishti-Ahuja@users.noreply.github.com>
2021-12-07 16:30:24 +0530

    Update settings.py

--- DiabetesPrediction/settings.py
+++ DiabetesPrediction/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'DiabetesPrediction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': str(BASE_DIR / "db.sqlite3"),
@@ -94,7 +94,7 @@ DATABASES = {
              'MARS_Connection': 'True',
          }
      }
- }
+ }'''
 
 
 # Password validation
