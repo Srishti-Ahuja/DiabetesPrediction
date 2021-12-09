b7605da5f2185013b79be1417bbd5af8c707aad2
Srishti Ahuja <57188326+Srishti-Ahuja@users.noreply.github.com>
2021-12-09 20:31:32 +0530

    Update settings.py

--- DiabetesPrediction/settings.py
+++ DiabetesPrediction/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
          'HOST': 'dbserverdjango.database.windows.net',
          'PORT': '1433',
          'OPTIONS' : {
-                'driver': 'ODBC Driver 13 for SQL Server',
+                'driver': 'ODBC Driver 17 for SQL Server',
           }
       }
  }
