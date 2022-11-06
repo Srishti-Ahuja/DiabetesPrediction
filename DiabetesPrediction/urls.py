4c37ebd716d255b7591a2c9b23102c7bf342f56f
Srishti Ahuja <57188326+Srishti-Ahuja@users.noreply.github.com>
2022-11-07 00:49:23 +0530

    Add static urls

--- DiabetesPrediction/urls.py
+++ DiabetesPrediction/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('admin/', admin.site.urls),
@@ -25,4 +28,4 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
